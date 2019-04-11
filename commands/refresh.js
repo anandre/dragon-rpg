b3b036a667c0b9f76e5e4a1edd69bc30cc1a0143
anandre <anandregit@gmail.com>
2019-04-11 08:07:37 -0500

    Create refresh.js

---
+++ commands/refresh.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+
+class RefreshCommand extends Command {
+    constructor() {
+        super('refresh', {
+            aliases: ['refresh'],
+            channel: 'guild'
+        })
+    }
+
+    async exec(message) {
+        if (!this.client.combat.has(message.author.id)) return message.channel.send('Only usable in combat!');
+        const combat = this.client.combat.get(message.author.id);
+        combat.currhp = combat.maxhp;
+        combat.currmp = combat.maxmp;
+
+        return message.channel.send(`${message.author.username}, your HP and MP have been refreshed!`);
+    };
+};
+
+module.exports = RefreshCommand;
